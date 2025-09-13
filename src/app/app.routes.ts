ba88dbf9f1b70da9ed694a326d53cfa1de7ebd69
gabrielmr012@gmail.com <gabrielmr012@gmail.com>
2025-09-13 11:50:16 -0300

    Desenvolvimento da tela de Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LoginComponentComponent } from './login-component/login-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponentComponent }
+];
